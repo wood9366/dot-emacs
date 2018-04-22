6cd42c8153e649dc2a3530f47f6704e33b2356db
Li Yang <7325560@qq.com>
2018-04-22 12:41:41 +0800

    * adjust edit :init config order.

--- lisp/config-edit.el
+++ lisp/config-edit.el
@@ -2,12 +2,12 @@
 ;; show parenthese
 (use-package highlight-parentheses
   :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-highlight-parentheses-mode))
   :config
   (custom-set-variables '(hl-paren-attributes (quote nil)))
   (custom-set-variables '(hl-paren-background-colors '("blue" "green" "yellow" "red")))
   (custom-set-variables '(hl-paren-colors (quote nil)))
-  :init
-  (add-hook 'after-init-hook 'global-highlight-parentheses-mode))
 
 (add-hook 'after-init-hook 'show-paren-mode)
 
