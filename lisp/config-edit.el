a8800c6aceeb441d664ba8020d23d08cadbd2bf4
Li Yang <7325560@qq.com>
2018-04-27 11:18:30 +0800

    replace highlight-parentheses with rainbow delimiter.

--- lisp/config-edit.el
+++ lisp/config-edit.el
@@ -1,13 +1,20 @@
 
 ;; show parenthese
-(use-package highlight-parentheses
+;; (use-package highlight-parentheses
+;;   :ensure t
+;;   :init
+;;   (add-hook 'after-init-hook 'global-highlight-parentheses-mode))
+;;   :config
+;;   (custom-set-variables '(hl-paren-attributes (quote nil)))
+;;   (custom-set-variables '(hl-paren-background-colors '("blue" "green" "yellow" "red")))
+;;   (custom-set-variables '(hl-paren-colors (quote nil)))
+
+(add-hook 'after-init-hook 'show-paren-mode)
+
+(use-package rainbow-delimiters
   :ensure t
   :init
-  (add-hook 'after-init-hook 'global-highlight-parentheses-mode))
-  :config
-  (custom-set-variables '(hl-paren-attributes (quote nil)))
-  (custom-set-variables '(hl-paren-background-colors '("blue" "green" "yellow" "red")))
-  (custom-set-variables '(hl-paren-colors (quote nil)))
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (add-hook 'after-init-hook 'show-paren-mode)
 
