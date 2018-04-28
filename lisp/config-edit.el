13db0623f6561acff062240a2ade0cdabfee458b
Li Yang <7325560@qq.com>
2018-04-28 16:28:34 +0800

    add basic edit config. add highlight escape mode.

--- lisp/config-edit.el
+++ lisp/config-edit.el
@@ -16,6 +16,17 @@
   :init
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
-(add-hook 'after-init-hook 'show-paren-mode)
+;; basic
+(setq-default
+ case-fold-search t ;; search or match ignore case
+ column-number-mode t ;; show column number
+ indent-tabs-mode nil ;; replace tab with space
+ default-tab-width 4) 
+
+;; highlight escape sequence
+(use-package highlight-escape-sequences
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'hes-mode))
 
 (provide 'config-edit)
