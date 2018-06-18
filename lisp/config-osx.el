9a3e646f2f9ec3f2dfc76c6429f75169e92bd7f7
Li Yang <7325560@qq.com>
2018-06-19 05:51:09 +0800

    add site-lisp load-path for mac os.

--- lisp/config-osx.el
+++ lisp/config-osx.el
@@ -1,5 +1,7 @@
 (when _mac_
   (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none))
+  (setq mac-option-modifier 'none)
+  (let ((default-directory  "/usr/local/share/emacs/site-lisp"))
+    (normal-top-level-add-subdirs-to-load-path)))
 
 (provide 'config-osx)
