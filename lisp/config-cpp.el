108fa108280664e34db4b40accf61cdc8e00cf94
Li Yang <7325560@qq.com>
2018-06-19 16:04:12 +0800

    adjust cpp key bind for specific keymap.

--- lisp/config-cpp.el
+++ lisp/config-cpp.el
@@ -1,8 +1,8 @@
 (require 'rtags)
 
-(defun rtags-config ()
+(defun rtags-config (keymap)
   ;; (rtags-start-process-unless-running)
-  (rtags-enable-standard-keybindings c-mode-base-map)
+  ;; (rtags-enable-standard-keybindings c-mode-base-map)
 
   (setq rtags-autostart-diagnostics t)
   (rtags-diagnostics)
@@ -10,25 +10,24 @@
 
   (add-to-list (make-local-variable 'company-backends) 'company-rtags)
   (setq-local company-idle-delay nil)
-  (define-key c-mode-base-map (kbd "M-/") 'company-complete)
 
-  (evil-define-key 'normal c-mode-base-map (kbd "g d") 'rtags-find-symbol-at-point)
-  (evil-define-key 'normal c-mode-base-map (kbd "g D") 'rtags-find-symbol)
-  (evil-define-key 'normal c-mode-base-map (kbd "g u") 'rtags-find-references-at-point)
-  (evil-define-key 'normal c-mode-base-map (kbd "g U") 'rtags-find-all-references-at-point)
-  (evil-define-key 'normal c-mode-base-map (kbd "g R") 'rtags-rename-symbol)
+  (evil-define-key 'normal keymap (kbd "g d") 'rtags-find-symbol-at-point)
+  (evil-define-key 'normal keymap (kbd "g D") 'rtags-find-symbol)
+  (evil-define-key 'normal keymap (kbd "g u") 'rtags-find-references-at-point)
+  (evil-define-key 'normal keymap (kbd "g U") 'rtags-find-all-references-at-point)
+  (evil-define-key 'normal keymap (kbd "g R") 'rtags-rename-symbol)
 
-  (evil-define-key 'normal c-mode-base-map (kbd "g }") 'rtags-next-match)
-  (evil-define-key 'normal c-mode-base-map (kbd "g {") 'rtags-previous-match)
-  (evil-define-key 'normal c-mode-base-map (kbd "g ]") 'rtags-location-stack-forward)
-  (evil-define-key 'normal c-mode-base-map (kbd "g [") 'rtags-location-stack-back)
+  (evil-define-key 'normal keymap (kbd "g }") 'rtags-next-match)
+  (evil-define-key 'normal keymap (kbd "g {") 'rtags-previous-match)
+  (evil-define-key 'normal keymap (kbd "g ]") 'rtags-location-stack-forward)
+  (evil-define-key 'normal keymap (kbd "g [") 'rtags-location-stack-back)
 
-  (evil-define-key 'normal c-mode-base-map (kbd "c c") 'projectile-compile-project)
-  (evil-define-key 'normal c-mode-base-map (kbd "c r") 'projectile-run-shell-command-in-root)
-  (evil-define-key 'normal c-mode-base-map (kbd "g l") 'rtags-imenu)
+  (evil-define-key 'normal keymap (kbd "c c") 'projectile-compile-project)
+  (evil-define-key 'normal keymap (kbd "c r") 'projectile-run-shell-command-in-root)
+  (evil-define-key 'normal keymap (kbd "g l") 'rtags-imenu)
 
-  (evil-define-key 'normal c-mode-base-map (kbd ", p o") 'projectile-find-other-file)
-  (evil-define-key 'normal c-mode-base-map (kbd ", p O") 'projectile-find-other-file-other-window)
+  (evil-define-key 'normal keymap (kbd ", p o") 'projectile-find-other-file)
+  (evil-define-key 'normal keymap (kbd ", p O") 'projectile-find-other-file-other-window)
 
   (use-package flycheck-rtags
     :config
@@ -43,8 +42,8 @@
   (c-set-style "k&r")
   (setq c-basic-offset 4))
 
-(add-hook 'c-mode-hook 'rtags-config)
-(add-hook 'c++-mode-hook 'rtags-config)
-(add-hook 'objc-mode-hook 'rtags-config)
+(add-hook 'c-mode-hook (lambda () (rtags-config c-mode-map)))
+(add-hook 'c++-mode-hook (lambda () (rtags-config c++-mode-map)))
+(add-hook 'objc-mode-hook (lambda () (rtags-config objc-mode-map)))
 
 (provide 'config-cpp)
