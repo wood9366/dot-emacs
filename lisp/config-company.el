55f7a00387f289f1bf1378d8ed4cfa22ce884108
liyang <7325560@qq.com>
2019-04-17 13:41:27 +0800

    add yasnippet compnay backend.

--- lisp/config-company.el
+++ lisp/config-company.el
@@ -26,6 +26,7 @@
 
   (setq company-backends
         '(company-capf
+          company-yasnippet
           company-files
           (company-dabbrev-code company-gtags company-etags company-keywords)
           company-dabbrev)))
