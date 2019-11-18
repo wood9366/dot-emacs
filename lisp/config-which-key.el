32b675937ef24ac29dbe78744fd7729147b79326
liyang <7325560@qq.com>
2019-11-18 12:56:16 +0800

    load which-key-mode during startup.

--- lisp/config-which-key.el
+++ lisp/config-which-key.el
@@ -1,10 +1,9 @@
 
 (use-package which-key
   :ensure t
-  :hook (after-init . which-key-mode)
-  :custom
-  (which-key-enable-extended-define-key t)
-  (which-key-idle-delay 0.5)
-  (which-key-popup-type 'minibuffer))
+  :config
+  (setq which-key-enable-extended-define-key t
+        which-key-idle-delay 0.5
+        which-key-popup-type 'minibuffer))
 
 (provide 'config-which-key)
