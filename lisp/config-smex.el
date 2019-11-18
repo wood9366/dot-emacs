b96c9ee66c073f70d47ce0ed9af7172bb70fed1b
liyang <7325560@qq.com>
2019-11-18 12:49:55 +0800

    defer load smex.

--- lisp/config-smex.el
+++ lisp/config-smex.el
@@ -1,7 +1,8 @@
+
 (use-package smex
   :ensure t
+  :defer t
   :config
-  ;; Change path for ~/.smex-items
   (setq-default smex-save-file (expand-file-name ".smex-items" user-emacs-directory)))
 
 (provide 'config-smex)
