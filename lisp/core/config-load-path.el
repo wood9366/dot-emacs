d5e18a92fc77aaf159d17fd44a8b2b03b05358ac
liyang <7325560@qq.com>
2019-11-20 17:35:32 +0800

    fix load path config.

--- lisp/core/config-load-path.el
+++ lisp/core/config-load-path.el
@@ -1,4 +1,4 @@
 
 (let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
-  (add-to-list 'load-path 'default-directory)
+  (add-to-list 'load-path default-directory)
   (normal-top-level-add-subdirs-to-load-path))
