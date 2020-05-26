ec9beb77fda973b73b9630bc2f3179b489c252e8
liyang <7325560@qq.com>
2020-05-26 10:14:11 +0800

    set plantuml output type to png. set plantuml jar path to brew path.

--- lisp/ly-plantuml.el
+++ lisp/ly-plantuml.el
@@ -1,13 +1,21 @@
 
+(defconst plantuml-jar "/usr/local/Cellar/plantuml/1.2020.9/libexec/plantuml.jar")
+
 (use-package plantuml-mode
   :ensure t
   :defer t
+  :init
+  (add-hook 'plantuml-mode-hook
+            (lambda ()
+              (setq plantuml-output-type "png")))
   :config
-  (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2018.12/libexec/plantuml.jar"))
+  (message "load plantuml mode")
+  (setq plantuml-jar-path plantuml-jar)
+  (setq plantuml-default-exec-mode 'jar))
 
 (use-package ob-plantuml
   :after org
   :config
-  (setq org-plantuml-jar-path "/usr/local/Cellar/plantuml/1.2018.12/libexec/plantuml.jar"))
+  (setq org-plantuml-jar-path plantuml-jar))
 
 (provide 'ly-plantuml)
